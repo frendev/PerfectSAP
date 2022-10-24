377d94aba9a718b0ecd7340ef83bff54c94cd0bc
Adwait Gore <adwait.vmgore@gmail.com>
2022-10-24 17:19:46 +0530

    changed wrong env vars

--- config/envs.tsx
+++ config/envs.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable prefer-destructuring */
 export const SENDINBLUE_API_KEY = process.env.SENDINBLUE_API_KEY;
-export const SENDINBLUE_TO_EMAIL = process.env.SENDINBLUE_API_KEY;
+export const SENDINBLUE_TO_EMAIL = process.env.SENDINBLUE_TO_EMAIL;
 
 /* eslint-disable prefer-destructuring */
-export const SENDINBLUE_TO_NAME = process.env.SENDINBLUE_API_KEY;
+export const SENDINBLUE_TO_NAME = process.env.SENDINBLUE_TO_NAME;
