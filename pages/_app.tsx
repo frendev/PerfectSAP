9c13d044e5b4f2937395e752f0f37522da8ab550
Adwait Gore <adwait.vmgore@gmail.com>
2022-11-14 01:40:25 +0530

    minor changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import SEO from "@bradgarropy/next-seo";
-import Layout from "../components/Layout";
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import SEO from '@bradgarropy/next-seo';
+import Layout from '../components/Layout';
 
 const queryClient = new QueryClient();
 
@@ -14,23 +14,23 @@ function MyApp({ Component, pageProps }: AppProps) {
           title="Perfect SAP"
           description="One stop solution for all your SAP needs."
           keywords={[
-            "sap",
-            "sap abap",
-            "sap hana",
-            "perectsap",
-            "erp",
-            "resource management",
-            "data processing",
-            "sap courses",
-            "best sap training",
-            "business intelligence",
+            'sap',
+            'sap abap',
+            'sap hana',
+            'perectsap',
+            'erp',
+            'resource management',
+            'data processing',
+            'sap courses',
+            'best sap training',
+            'business intelligence',
           ]}
           facebook={{
-            image: "public\\assets\\PerfectSAP-Logo.png",
-            url: "http://www.perfectsap.com",
-            type: "website",
+            image: 'public\\assets\\PerfectSAP-Logo.png',
+            url: 'http://www.perfectsap.com',
+            type: 'website',
           }}
-        ></SEO>
+        />
         <Layout>
           <Component {...pageProps} />
         </Layout>
